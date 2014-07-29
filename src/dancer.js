e89294be4d2b760bd7e2a41b5fd6d6cae274a7ee
Hack Reactor Students <danthareja@gmail.com>
2014-07-28 20:56:20 -0700

    Basic Dance Off functionality added! OH SNAP.

--- src/dancer.js
+++ src/dancer.js
@@ -3,6 +3,7 @@ var Dancer = function(top, left, timeBetweenSteps) {
   this.timeBetweenSteps = timeBetweenSteps;
   this.step();
   this.setPosition(top, left);
+  this.getAtMe();
 };
 
 Dancer.prototype.step = function() {
@@ -17,6 +18,32 @@ Dancer.prototype.setPosition = function(top, left) {
   };
   this.$node.css(styleSettings);
 };
-Dancer.prototype.lineUp = function(left){
-  this.$node.css('left', left);
+Dancer.prototype.getAtMe = function(){
+  for (var i = 0; i<window.dancers.length; i++){
+    if (!(this instanceof dancers[i].constructor)){
+      if (Math.abs(this.$node.offset().top - dancers[i].$node.offset().top) < 300){
+        var height = $('body').height() * .8;
+        var width = $('body').width();
+        this.setPosition(height, width * .4);
+        dancers[i].setPosition(height, width * .6);
+        var lineUp = function(){
+          var intervalWidth = $("body").width() / dancers.length;
+          var dancerHeight = $("body").height()*0.55;
+          // iterate over dancers array
+          for (var j = 0; j < dancers.length-1; j++) {
+            if (j !== i){
+              dancers[j].setPosition(dancerHeight, intervalWidth*j);
+            }
+          }
+        }();
+      }
+    }
+  }
 };
+//on invocation
+//iterate through dancer array
+  //if type === different
+  //check distance
+  //  //if distance < limit
+  //    DANCE OFF MOFO
+
